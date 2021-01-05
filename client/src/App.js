e67e9e7bf1f8292f9412182d854f4282fa4c29ed
Chambers Mansour <chambersmansour97@gmail.com>
2021-01-04 18:08:45 -0600

    added components

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
 import Header from './components/Header'
+import Books from './components/Books'
 import './App.css';
 
 
@@ -8,6 +9,7 @@ class App extends Component {
     return (
       <div className="App">
         <Header />
+        <Books />
       </div>
     );
   }
---
+++ client/src/components/SearchArea.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const SearchArea = () => {
+    return(
+        <div className="serach-area">
+            <form action="">
+                <input type="text"></input>
+                <button type="submit">Search</button>
+            </form>
+        </div>
+    )
+}
+
+export default SearchArea;
